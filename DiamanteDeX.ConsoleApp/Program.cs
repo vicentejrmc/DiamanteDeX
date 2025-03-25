25a1c0689cfe35ae1c56125ca98d0c00265c5724
vicentejrmc <vicente_pc@outlook.com>
2025-03-25 16:10:28 -0300

    Implementada 1° Fase do Projeto Requisitos: 1 - Entrada de dados, 2 - Validação de entrada, 3 - Solicitar novo numero caso ítem (2) não seja atendido.

--- DiamanteDeX.ConsoleApp/Program.cs
+++ DiamanteDeX.ConsoleApp/Program.cs
@@ -2,15 +2,39 @@
 {
     internal class Program
     {
+        // 1° Fase do Projeto.
+        //Permitir ao usuário digitar um numero impar como entrada de dados.
+        //Fazer a Validação da especificação do numero: (Aceitar somente números impares)
+        //Caso a entrada dada pelo usuário não atenda a especificação do ponto 2, solicitar nova entrada antes de prosseguir.
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("-------------------------");
             Console.WriteLine("----- Diamante de X -----");
-            Console.WriteLine("-------------------------");
+            Console.WriteLine("-------------------------\n");
+
+            Console.WriteLine("Digite um número ímpar: ");
+            int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+            ValidarNumeroImpar(numeroDigitado);
 
 
 
             Console.ReadLine();
+        }
+
+        public static int ValidarNumeroImpar(int numeroDigitado)
+        {
+            bool numeroImpar = numeroDigitado % 2 == 0;
+
+            while (numeroImpar)
+            {
+                Console.WriteLine("O numero digitado é invalido! digite um numero ÍMPAR: ");
+                numeroDigitado = Convert.ToInt32(Console.ReadLine());
+                numeroImpar = numeroDigitado % 2 == 0;
+            }
+            return numeroDigitado;
         }
     }
 }
