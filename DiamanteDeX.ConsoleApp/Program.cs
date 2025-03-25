43ee460ddd98287be8b16410a94a54a6c23dac54
vicentejrmc <vicente_pc@outlook.com>
2025-03-25 15:44:02 -0300

    Add project files.

---
+++ DiamanteDeX.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DiamanteDeX.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("----- Diamante de X -----");
+            Console.WriteLine("-------------------------");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
