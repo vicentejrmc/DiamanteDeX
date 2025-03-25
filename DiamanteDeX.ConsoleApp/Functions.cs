125ff435b00470c7a147a7c16df015ac22b75438
vicentejrmc <vicente_pc@outlook.com>
2025-03-25 17:37:57 -0300

    Estruturação de Classe: Functions

--- DiamanteDeX.ConsoleApp/Functions.cs
+++ DiamanteDeX.ConsoleApp/Functions.cs
@@ -15,5 +15,46 @@
             return numeroDigitado;
         }
 
+
+        public static int NumeroEhPositivo(int numeroDigitado)
+        {
+            bool numeroEhPositivo = numeroDigitado < 0;
+
+            while (numeroEhPositivo)
+            {
+                Console.Write("Numero Invalido! Digite um numero Ímpar Positivo: ");
+                numeroDigitado = Convert.ToInt32(Console.ReadLine());
+            }
+
+            return numeroDigitado;
+        }
+
+        public static void ApresentarDiamante(int numeroDigitado)
+        {
+            int espacoEmBranco = numeroDigitado / 2;
+            int escreverX = 1;
+            for (int i = 0; i < numeroDigitado; i++)
+            {
+                for (int j = 0; j < espacoEmBranco; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < escreverX; j++)
+                {
+                    Console.Write("X");
+                }
+                Console.WriteLine();
+                if (i < numeroDigitado / 2)
+                {
+                    espacoEmBranco--;
+                    escreverX += 2;
+                }
+                else
+                {
+                    espacoEmBranco++;
+                    escreverX -= 2;
+                }
+            }
+        }
     }
 }
--- DiamanteDeX.ConsoleApp/Program.cs
+++ DiamanteDeX.ConsoleApp/Program.cs
@@ -17,40 +17,17 @@
             Console.Write("Digite um número ímpar: ");
             int numeroDigitado = Convert.ToInt32(Console.ReadLine());
 
+            Functions.NumeroEhPositivo(numeroDigitado);
+
             Functions.ValidarNumeroImpar(numeroDigitado);
 
-            ApresentarDiamante(numeroDigitado);
+            Functions.ApresentarDiamante(numeroDigitado);
 
             Console.ReadLine();
         }
 
-        public static void ApresentarDiamante(int numeroDigitado)
-        {
-            int espacoEmBranco = numeroDigitado / 2;
-            int escreverX = 1;
-            for (int i = 0; i < numeroDigitado; i++)
-            {
-                for (int j = 0; j < espacoEmBranco; j++)
-                {
-                    Console.Write(" ");
-                }
-                for (int j = 0; j < escreverX; j++)
-                {
-                    Console.Write("X");
-                }
-                Console.WriteLine();
-                if (i < numeroDigitado / 2)
-                {
-                    espacoEmBranco--;
-                    escreverX += 2;
-                }
-                else
-                {
-                    espacoEmBranco++;
-                    escreverX -= 2;
-                }
-            }
-        }
+        
+       
 
     }
 }
